c942a532a57fb1cf272570574890a3d7c914c56e
AneeshBoreda <aneesh.boreda92@gmail.com>
2020-09-19 03:13:47 -0400

    hi

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_socketio import SocketIO
 import pickle
 from chat import chat
 
-db=pickle.load(open('data.pkl','r'))
+db=pickle.load(open('data.pkl','rb'))
 app = Flask(__name__, static_url_path='/static', template_folder='templates')
 app.secret_key='aj34$&8@j!#PO!@#$'
 socketio = SocketIO(app)
