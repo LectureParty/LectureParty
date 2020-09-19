7a63ca39ce1cade9e344f7e5f4a7cd67a96dcebd
AneeshBoreda <aneesh.boreda92@gmail.com>
2020-09-19 17:22:42 -0400

    most of webssite should work

--- app.py
+++ app.py
@@ -3,17 +3,19 @@ from flask_socketio import SocketIO
 import pickle
 import random
 db={}
-users,rooms,rbu=None,None,None
+users,rooms,rbu,ubr=None,None,None,None
 try:
     db=pickle.load(open('data.pkl','rb'))
     users=db['users']
     rooms=db['rooms']
     rbu=db['rooms_by_user']
+    ubr=db['users_by_room']
 except:
-    db={'users': dict(),'rooms':dict(),'rooms_by_user':dict()}
+    db={'users': dict(),'rooms':dict(),'rooms_by_user':dict(),'users_by_room':dict()}
     users=db['users']
     rooms=db['rooms']
     rbu=db['rooms_by_user']
+    ubr=db['users_by_room']
 
 
 app = Flask(__name__, static_url_path='/static', template_folder='templates')
@@ -82,6 +84,8 @@ def new_party():
     name=request.form['create-name']
     room_id=random.randint(10**9,9*(10**9))
     rbu[session['username']].append(room_id)
+    ubr[room_id]=list()
+    ubr[room_id].append(session['username'])
     rooms[room_id]=name
     pickle.dump(db,open('data.pkl','wb'))
     return redirect(url_for('party',party_id=room_id))
@@ -95,6 +99,7 @@ def join_party():
     
     if code in rooms:
         rbu[session['username']].append(code)
+        ubr[code].append(session['username'])
         pickle.dump(db,open('data.pkl','wb'))
         return redirect(url_for('party',party_id=code))
     else:
@@ -103,13 +108,18 @@ def join_party():
 @app.route('/party/<int:party_id>')
 def party(party_id):
     if party_id in rooms:
-        return render_template('lecture-info.html',**{'name': rooms[party_id], 'code': party_id})
+        return render_template('lecture-info.html',**{'name': rooms[party_id], 'code': party_id,'participants':ubr[party_id]})
     else:
         return 'Room not found.'
 
 @app.route('/partymeet/<int:party_id>')
 def partymeet(party_id):
     return render_template('party.html',**{'roomnumber':party_id, 'username': session['username']})
+
+@app.route('/log_out')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
