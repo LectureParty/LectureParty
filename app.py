3c19bad12514c38d228839d116c792151b47a0a6
EC2 Default User <ec2-user@ip-172-31-1-194.us-east-2.compute.internal>
2020-09-19 05:38:35 +0000

    hi

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import *
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static', template_folder='templates')
 @app.route('/')
-def hello_world():
-    return 'Hello from Mikail!'
+def index():
+    return render_template('base.html')
+@app.route('/test/<path:path>')
+def test(path):
+    return render_template(path)
 if __name__ == '__main__':
     app.run()
