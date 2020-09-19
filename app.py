92d43b533cc4876372e150dd179e423e4793d6ec
AneeshBoreda <aneesh.boreda92@gmail.com>
2020-09-19 03:02:04 -0400

    added endpoints

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import *
 from flask_socketio import SocketIO
+import pickle
+from chat import chat
 
+db=pickle.load(open('data.pkl','r'))
 app = Flask(__name__, static_url_path='/static', template_folder='templates')
+app.secret_key='aj34$&8@j!#PO!@#$'
 socketio = SocketIO(app)
 
-from chat import chat
+
 app.register_blueprint(chat)
 
 @app.route('/')
@@ -14,6 +18,14 @@ def index():
 @app.route('/test/<path:path>')
 def test(path):
     return render_template(path)
+
+@app.route('/auth',methods=['POST'])
+def auth():
+    return request.form
+
+@app.route('/create_account',methods=['POST'])
+def create():
+    return request.form
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
