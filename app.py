344216d5f4cf3123f46e8c9235a0367f48e3aed0
AneeshBoreda <aneesh.boreda92@gmail.com>
2020-09-19 15:21:24 -0400

    server allow creation of parties

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import *
 from flask_socketio import SocketIO
 import pickle
+import random
 db={}
+users,rooms,rbu=None,None,None
 try:
     db=pickle.load(open('data.pkl','rb'))
-    
+    users=db['users']
+    rooms=db['rooms']
+    rbu=db['rooms_by_user']
 except:
-    db={'users': dict(),'rooms':dict()}
-users=db['users']
-rooms=db['rooms']
+    db={'users': dict(),'rooms':dict(),'rooms_by_user':dict()}
+    users=db['users']
+    rooms=db['rooms']
+    rbu=db['rooms_by_user']
+
 
 app = Flask(__name__, static_url_path='/static', template_folder='templates')
 app.secret_key='aj34$&8@j!#PO!G@#$'
@@ -48,6 +54,7 @@ def auth():
 def update_pw():
     p=request.form['password'] 
     users[session['username']]=p
+    pickle.dump(db,open('data.pkl','wb'))
     return redirect(url_for('index'))
 
 @app.route('/create_account')
@@ -62,17 +69,26 @@ def create():
         return redirect(url_for('create_account',error=True))
     else:
         users[u]=p
+        rbu[u]=list()
         session['username']=u
         pickle.dump(db,open('data.pkl','wb'))
         return redirect(url_for('index'))
     
 @app.route('/new_party')
 def new_party():
-    name=request.forms['party_name']
-    pw=request.forms['party_pwd']
-    rooms['name']=pw
+    name=request.forms['create-name']
+    room_id=random.randint(10**9,9*(10**9))
+    rbu[session['username']].append(room_id)
+    rooms[room_id]=name
 
 @app.route('/party/<int:party_id>')
+def party(party_id):
+    if party_id in rooms:
+        return render_template('lecture-info.html',**{'name': rooms[party_id], 'code': party_id})
+    else:
+        return 'Room not found.'
+
+@app.route('/partymeet/<int:party_id>')
 def party(party_id):
     return render_template('party.html',**{'roomnumber':party_id, 'username': session['username']})
 
