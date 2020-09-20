296af694d8bfef170b2b331bc4e504e9ed6492f8
Emily Y <emilygbs@yahoo.com>
2020-09-20 02:52:12 -0400

    modified scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,7 +1,10 @@
 from Collections import Counter
 
-def scheduler(num_meetings, times):
-    cnt = Counter()
-    for time in times:
-        cnt[time] += 1
-    return cnt.most_common(num_meetings)
+def scheduler(bigArray, cnt=None):
+    if cnt == None:
+        cnt = Counter()
+    for i in bigArray:
+        if i != 0:
+            for j in bigArray[i]:
+                cnt[(i, j)] += 1
+    return cnt.most_common();
