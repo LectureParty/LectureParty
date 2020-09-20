24b341bc3036471c0259d2277e7182928ebf793a
Emily Y <emilygbs@yahoo.com>
2020-09-20 02:54:48 -0400

    changed to indices

--- scheduler.py
+++ scheduler.py
@@ -3,8 +3,8 @@ from Collections import Counter
 def scheduler(bigArray, cnt=None):
     if cnt == None:
         cnt = Counter()
-    for i in bigArray:
+    for i in range(len(bigArray)):
         if i != 0:
-            for j in bigArray[i]:
+            for j in range(len(bigArray[i])):
                 cnt[(i, j)] += 1
-    return cnt.most_common();
+    return cnt.most_common()
